e3a121db4768cc6a888ce91aa7dacc9c29c551c7
Temidayo <temideewan@gmail.com>
2022-07-24 20:12:37 +0100

    add background toggle with dice roll

--- lib/main.dart
+++ lib/main.dart
@@ -26,32 +26,52 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
   Random randomNum = Random();
+  Color _selectedColor;
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.grey,
+    Colors.black87,
+    Colors.yellow
+  ];
 
   void _rollDice() {
     setState(() {
       leftDiceNumber = randomNum.nextInt(6) + 1;
       rightDiceNumber = randomNum.nextInt(6) + 1;
+      _selectedColor = colors[randomNum.nextInt(6)];
     });
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _selectedColor = colors[randomNum.nextInt(6)];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: TextButton(
-              onPressed: _rollDice,
-              child: Image.asset("images/dice$leftDiceNumber.png"),
+    return Container(
+      color: _selectedColor,
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextButton(
+                onPressed: _rollDice,
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+              ),
             ),
-          ),
-          Expanded(
-            child: TextButton(
-              onPressed: _rollDice,
-              child: Image.asset("images/dice$rightDiceNumber.png"),
+            Expanded(
+              child: TextButton(
+                onPressed: _rollDice,
+                child: Image.asset("images/dice$rightDiceNumber.png"),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
